0a8083b232a8e3996cbd491dc52ce6a06d4794b0
Farshad Bolouri <bolouri.farshad@gmail.com>
2019-12-10 22:18:56 +0330

    teesting unpacking feature in python

---
+++ example/Unpacking-list-tuple.py
@@ -0,0 +1,6 @@
+numbers = [1, 2, 3, 5, 100]
+a, b, c, z, x = numbers
+week = ("sat", "sun", "mon", "tue", "wed", "thu", "fri")
+d1, d2, d3, d4, d5, d6, d7 = week
+print(a, b, c, z, x)
+print(d1, d2, d3, d4, d5, d6, d7)
