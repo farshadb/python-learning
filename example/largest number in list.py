4194bc41ad6da4da7808c64509a30b79a60a9aa8
Farshad Bolouri <bolouri.farshad@gmail.com>
2019-12-10 12:57:32 +0330

    crreate matrix file and start using 2 dimentional list in python, and modified largest ... app

--- example/largest number in list.py
+++ example/largest number in list.py
@@ -1,10 +1,10 @@
 numbers = [2, 6, 8, 9, 12, 3, 100, 45, 50, 1, 0, 120, 1000, -2]
 min_number = 0
 max_number = 0
-for index in numbers:
-    if index > max_number:
-        max_number = index
-    if index < min_number:
-        min_number = index
+for number in numbers:
+    if number > max_number:
+        max_number = number
+    if number < min_number:
+        min_number = number
 print(f'maximum: {max_number}')
 print(f'minimum: {min_number}')
