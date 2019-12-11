96fc7817af95f362a47a12af29c3a65749c063cf
Farshad Bolouri <bolouri.farshad@gmail.com>
2019-12-11 18:14:06 +0330

    pperson class

---
+++ example/simple-class.py
@@ -0,0 +1,11 @@
+class Person:
+
+    def __init__(self, name):
+        self.name = name
+    def talk(self):
+        print(f"Hi, I'm {self.name}")
+
+
+john = Person("John Smith")
+print(john.name)
+
