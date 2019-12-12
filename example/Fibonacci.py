ab38a452d6aa334ebb2576073dd52a23492f1149
Farshad Bolouri <bolouri.farshad@gmail.com>
2019-12-12 13:17:17 +0330

    Fibonacci profecinal module added

---
+++ example/Fibonacci.py
@@ -0,0 +1,19 @@
+# Fibonacci numbers module
+
+
+def fibonacci_write(n):    # write Fibonacci series up to n
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+
+
+def fibonacci_return(n):   # return Fibonacci series up to n
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
+
